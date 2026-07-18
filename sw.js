56a86b74e33143c5b2595cbdc96132ba876d764e
Rumimaki <tiaraearth@gmail.com>
2026-07-18 13:17:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 星空メモ Service Worker — オフライン起動・キャッシュ・リマインダー通知 */
-const VER = "hoshizora-v4";
+const VER = "hoshizora-v5";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
